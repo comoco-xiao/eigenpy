c6a37285e94e56ca71cd8012cd3b2b17c2629446
Xiao Zhang <xiaoz@comoco.xyz>
2025-03-22 13:28:13 +0800

    update

---
+++ include/eigenpy/test-eigenpy.hpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2014-2019, CNRS
+ * Copyright 2018-2024, INRIA
+ */
+
+#ifndef __test_eigenpy_hpp__
+#define __test_eigenpy_hpp__
+
+namespace eigenpy {
+  void testPyBinding();
+}
+#endif  // ifndef __test_eigenpy_hpp__
